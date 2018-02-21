df170f17f74f3a18e20d292f51098ee013ac94cd
Ecconia <github-commit@ecconia.com>
2018-02-21 22:59:39 +0100

    Removing import

--- src/main/java/com/github/mastercake10/telegramchat/TelegramChatPlugin.java
+++ src/main/java/com/github/mastercake10/telegramchat/TelegramChatPlugin.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.java.JavaPlugin;
